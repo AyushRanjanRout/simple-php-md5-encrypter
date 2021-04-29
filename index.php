3571543123eeac704a19640693ab1a8f48645d68
Ayush Ranjan Rout Rout <57044707+AyushRanjanRout@users.noreply.github.com>
2021-04-29 12:04:07 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+
+if(isset($_POST['md5'])){
+
+$md5 = md5($_POST['md5']);
+
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MD5 Encrypt</title>
+    <style>
+        .form{
+            margin:auto;
+            display:block;
+        }
+
+        .input_box{
+            width:50%;
+            padding: 10px;
+            border-radius:50px;
+            border: 3px solid purple;
+        }
+        .submit_box{
+            width:25%;
+            padding: 10px;
+            border-radius:50px;
+            border: 3px solid purple;
+            color: white;
+            background: purple;
+        }
+    </style>    
+</head>
+<body>
+    <form action="index.php" method="post" class="form">
+        <input class="input_box" type="text" placeholder="Enter Text Here " name="md5">
+        <button class="submit_box" type="submit">Encrypt</button>
+    </form>
+   <?php 
+    if(isset($_POST['md5'])){
+     //echo $_POST['md5'] . $md5;
+     echo "<p>Your Hash is :<b> $md5</b></p>";
+    }
+    ?>
+  
+    
+</body>
+</html>
